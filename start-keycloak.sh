ab3bbd4cf6dca00ca4a442be71217af7ce45cfc5
Yesveer <80391289+Yesveer@users.noreply.github.com>
2025-04-21 22:52:25 +0530

    Update start-keycloak.sh

--- start-keycloak.sh
+++ start-keycloak.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Set default port if not provided by Render
-PORT=${PORT:-8080}
-
-# Start Keycloak in development mode using environment variables
-/opt/keycloak/bin/kc.sh start-dev --http-port=$PORT
+/opt/keycloak/bin/kc.sh start-dev --http-port=8000
